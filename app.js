fe2192aa7da9542fb7c06cf6e18b8c94d0d5a1f5
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-27 05:30:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router.js';
+import './app.css';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
 
