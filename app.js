852f13758cbf62e480695ee1247852ca2964a352
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-28 03:34:20 +0000

    7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
-import './app.css';
+import './Style/app.css';
 
 const app = createApp(App);
 app.use(router);
